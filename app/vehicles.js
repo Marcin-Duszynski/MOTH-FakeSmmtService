89ffba6984e270aff330661eb2a539833df29a80
Marcin-Duszynski <marcin@duszynski.pl>
2017-10-06 14:09:15 +0200

    Vincheck endpoint with basic scenarios implementation

--- app/vehicles.js
+++ app/vehicles.js
@@ -1,13 +1,53 @@
 'use strict'
 
+/*
+Recalls list created based on test data obtained during real dev SMMT service tests.
+More details in JIRA: BL-6009
+*/
+
 var vehiclesDictionary = {
-    "AISXXXTEST1239607-BRUIN": {
-        "status": 201,
-        "status_description": "Recall Outstanding",
-        "vin": "AISXXXTEST1239607",
-        "vin_recall_status": "BRAKES",
+    "BRUIN": {
+        "AISXXXTEST1239607": {
+            "status": 201,
+            "status_description": "Recall Outstanding",
+            "vin": "AISXXXTEST1239607",
+            "vin_recall_status": "BRAKES",
+            "last_update": "19022015"
+        }
+    }
+};
+
+function invalidMarque(vin){
+    return {
+        "status": 402,
+        "status_description": "Bad Request - Invalid Marque",
+        "vin": `${vin}`,
+        "vin_recall_status": "",
+        "last_update": ""
+    }
+};
+
+function unknownVin(vin){
+    return {
+        "status": 200,
+        "status_description":"No Recall Outstanding",
+        "vin": `${vin}`,
+        "vin_recall_status": "",
         "last_update": "19022015"
     }
 };
 
-exports.getRecall = (vin, make) => vehiclesDictionary[`${vin}-${make}`];
+exports.getRecall = (vin, marque) => {
+    var vehiclesWithMarque = vehiclesDictionary[marque.toUpperCase()];
+
+    if (vehiclesWithMarque === undefined) {
+        return invalidMarque(vin);
+    };
+
+    var vehicle = vehiclesWithMarque[vin.toUpperCase()];
+    if (vehicle === undefined) {
+        return unknownVin(vin);
+    };
+
+    return vehicle;
+}
--- app/vincheck.js
+++ app/vincheck.js
@@ -4,12 +4,21 @@ const vehicles = require('./vehicles');
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 app.get('/health', function(req, res){
     res.status(200).send('Up and running.');
 });
 
 app.post('/vincheck', function(req, res){
-    var vehicle = vehicles.getRecall('AISXXXTEST1239607', 'BRUIN');
+    console.log("Request body:");
+    console.log(req.body);
+
+    var vehicle = vehicles.getRecall(req.body['VIN'], req.body['Marque']);
+    console.log("Response body:");
+    console.log(vehicle);
+
     res.send(vehicle);
 })
 
